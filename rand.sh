6e14315bf65b676efeeb6f796a0c87b324840076
hashnoncemessage <hashnoncemessage@proton.me>
2024-04-02 12:26:38 +0200

    fix

--- rand.sh
+++ rand.sh
@@ -5,8 +5,8 @@ echo "random file is $F"
 
 echo "<img src='./img/$F' style='width:100%'>" > readme.md
 
-#git config --global user.name hashnoncemessage
-#git config --global user.email hashnoncemessage@proton.me
-#git add readme.md
-#git commit -m update
-#git push
+git config --local user.name hashnoncemessage
+git config --local user.email hashnoncemessage@proton.me
+git add readme.md
+git commit -m "update to $F"
+git push origin main
