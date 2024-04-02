746295fe6a848148fc15f101c1fc7cd08e7ffe2f
hashnoncemessage <hashnoncemessage@proton.me>
2024-04-02 12:42:47 +0200

    wip

--- rand.sh
+++ rand.sh
@@ -4,9 +4,3 @@ F=$(ls img | shuf -n 1)
 echo "random file is $F"
 
 echo "<img src='./img/$F' style='width:100%'>" > readme.md
-
-git config --local user.name github-actions
-git config --local user.email github-actions@github.com
-git add readme.md
-git commit -m "update to $F"
-git push origin main
